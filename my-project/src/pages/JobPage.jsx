ee03d0b8576afa08d387fc85e2496745e052f012
Annon_101 <67450280+Briankurtish@users.noreply.github.com>
2024-09-05 14:53:17 +0100

    use Dataloaders to get data on the Job page

--- my-project/src/pages/JobPage.jsx
+++ my-project/src/pages/JobPage.jsx
@@ -1,10 +1,16 @@
-import { useParams } from "react-router-dom";
-import Spinner from "../components/Spinner";
+import { useParams, useLoaderData } from "react-router-dom";
 
 const JobPage = () => {
   const { id } = useParams();
+  const job = useLoaderData();
 
-  return loading ? <Spinner /> : <h1>{job.title}</h1>;
+  return <h1>{job.title}</h1>;
 };
 
-export default JobPage;
+const jobLoader = async ({ params }) => {
+  const res = await fetch(`/api/jobs/${params.id}`);
+  const data = await res.json();
+  return data;
+};
+
+export { JobPage as default, jobLoader };
